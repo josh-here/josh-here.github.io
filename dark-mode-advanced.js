b73b1c96ef478b7e0a543a503032a8d2565e267e
josh-here <79751398+josh-here@users.noreply.github.com>
2021-03-16 16:30:28 +0530

    Add files via upload

--- dark-mode-advanced.js
+++ dark-mode-advanced.js
@@ -3,14 +3,15 @@
  * Demo for https://www.bram.us/2020/04/26/the-quest-for-the-perfect-dark-mode-using-vanilla-javascript/
  */
 
+
 const setColorMode = (mode) => {
 	// Mode was given
 	if (mode) {
 		// Update data-* attr on html
 		document.documentElement.setAttribute('data-force-color-mode', mode);
 		// Persist in local storage
-		/**window.**/localStorage.setItem('color-mode', mode);
-        Cookies.set('color-mode', mode, { expires:Infinity });
+		window.localStorage.setItem('color-mode', mode);
+        //Cookies.set('color-mode', mode);
 		// Make sure correct radio button is up-to-date
 		document.querySelector('#force-color-mode-to-dark').checked = (mode === 'dark');
 		document.querySelector('#force-color-mode-to-light').checked = (mode === 'light');
@@ -31,8 +32,8 @@ const setColorMode = (mode) => {
 		// Remove data-* attr from html
 		document.documentElement.removeAttribute('data-force-color-mode');
 		// Remove entry from local storage
-		/**window.**/localStorage.removeItem('color-mode');
-		Cookies.expire('color-mode');
+		window.localStorage.removeItem('color-mode');
+		//Cookies.expire('color-mode');
 		// Make sure the checkbox is up-to-date, matching the system preferences
 		document.querySelector('#auto-color-mode').checked = true;
     
@@ -49,7 +50,7 @@ const setColorMode = (mode) => {
 
 
 
-window.onload=function(){
+
 document.querySelector('#force-color-mode-to-light').addEventListener('click', (e) => {
 	setColorMode('light');
 });
@@ -61,18 +62,18 @@ document.querySelector('#force-color-mode-to-dark').addEventListener('click', (e
 document.querySelector('#auto-color-mode').addEventListener('click', (e) => {
 	setColorMode(false);
 });
-}
+
 
 
 
 
 // FOR TOGGLE
 
-window.onload=function(){
+
 document.querySelector('#toggle-darkmode').addEventListener('click', (e) => {
 	setColorMode(e.target.checked ? 'dark' : 'light');
 });
-}
+
 // Keep an eye out for System Light/Dark Mode Changes
 const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 mediaQuery.addListener(() => {
@@ -84,3 +85,5 @@ mediaQuery.addListener(() => {
 	// Make sure the checkbox is up-to-date
  	document.querySelector('#toggle-darkmode').checked = mediaQuery.matches;
 });
+
+
