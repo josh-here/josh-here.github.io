a9439081063cc36ff3a26026b8b7dcd0a451d577
josh-here <79751398+josh-here@users.noreply.github.com>
2021-03-16 10:40:51 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,34 +1,65 @@
-function swapStyleSheet(sheet){
-	document.getElementById('accent-color').setAttribute('href', sheet);
-}
+$(document).ready(function(){
+
+    // ==============
+    // theme switcher
 
 
-
-
-    $('#color-1').click(function () {
-      // stylesheet stuff...
-      createCookie('cookie','primary',1);
-    });
-    $('#color-2').click(function () {
-      // stylesheet stuff...
-      createCookie('cookie','indigo',1);
-    }); 
-    $('#color-3').click(function () {
-      // stylesheet stuff...
-      createCookie('cookie','rose',1);
-    }); 
-  
-
-
-    $(document).ready(function(){
-      updateResult();
+    // Original : http://jamesmathias.com/blog/article/switching-styles-silently-in-six-steps
+    // ==============
+    
+        // listen for clicks on the a links within .themes
+        $('.accent-radios input').on('click',function(){
+            // change the href of link.theme to match the rel of this
+            $('.theme').attr('href',$(this).attr('rel'));
+            // set a cookie to remember
+           // $.cookie('theme-cookie',$(this).attr('rel'), {expires: 999999});
+            Cookies.set('theme-cookie',$(this).attr('rel'), {expires: Infinity});
+            // prevent this from reloading the browser
+            return true;
+        });
+    
     });
 
-    function updateResult() {
-      var cookieValue = getCookie('cookie');
-      if (cookieValue.length) {
-        $('#accent-color').attr('href', 'assets/css/' + cookieValue + '.css');
-      }
-    }
+
+    $(function() {
+
+        $("input[name=\"color\"]").click(function(){
+            var thisElem = $(this);
+            var value = thisElem.val();
+
+            //localStorage:
+            //localStorage.setItem("option", value);
+            //Cookies:
+            Cookies.set('option',value, {expires: Infinity});
+            //document.cookie="option="+value;
+        });
+        /**
+        //localStorage:
+        var itemValue = localStorage.getItem("option");
+        if (itemValue !== null) {
+            $("input[value=\""+itemValue+"\"]").click();
+        }
+        */
+         /**
+        //Cookies:
+        var n = document.cookie;
+
+        if (n.indexOf("option=") !== -1) {
+            var cookieValue = n.substring(n.indexOf("option=")+7, n.indexOf(";"))
+            $("input[value=\""+cookieValue+"\"]").click();
+        }
+        **/
+
+        const cookiename = Cookies.get('option'); // "value"
+        if(cookiename === 'color-1'){
+            $("#color-1").prop("checked", true);
+        } else if(cookiename === 'color-2'){
+            $("#color-2").prop("checked", true);
+        } else if(cookiename === 'color-3'){
+            $("#color-3").prop("checked", true);
+        }
+        
+    });
+
 
 
